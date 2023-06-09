e7e65dff1ccb2b9e68f13f963a4ad5dbb3d87557
Yassen <nwbyaseen@gmail.com>
2023-06-10 02:59:48 +0500

    complete

---
+++ script.js
@@ -0,0 +1,22 @@
+setInterval(() => {
+    // d = new Date();
+    // htime = new d.getHours();
+    // mtime = new d.getMinutes();
+    // stime = new d.getSeconds();
+    // hrotation = 30*htime + mtime/2;
+    // mrotation = 6*mtime;
+    // srotation = 6*stime; 
+    // hour.style.transform = `rotate(${hrotation}deg)`;
+    // mins.style.transform = `rotate(${mrotation}deg)`;
+    // sec.style.transform = `rotate(${srotation}deg)`;
+    d = new Date();
+    htime =  d.getHours();
+    mtime =  d.getMinutes();
+    stime =  d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    mins.style.transform = `rotate(${mrotation}deg)`;
+    sec.style.transform = `rotate(${srotation}deg)`;
+},  1000);
